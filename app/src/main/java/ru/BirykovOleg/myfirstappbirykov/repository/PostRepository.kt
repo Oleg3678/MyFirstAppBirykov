440ad00cd7f6f159cbcf1e7c43b8bbd7d007378a
Oleg3678 <olegvlogs9@gmail.com>
2026-03-19 14:30:31 +0300

    Day 8: Material Design styling for buttons

---
+++ app/src/main/java/ru/BirykovOleg/myfirstappbirykov/repository/PostRepository.kt
@@ -0,0 +1,16 @@
+package ru.BirykovOleg.myfirstappbirykov.repository
+
+
+
+import androidx.lifecycle.LiveData
+import ru.BirykovOleg.myfirstappbirykov.dto.Post
+
+interface PostRepository {
+    fun getAll(): LiveData<List<Post>>
+    fun likeById(id: Long)
+    fun shareById(id: Long)
+    fun increaseViews(id: Long)
+    fun save(post: Post)        // для создания и обновления
+    fun removeById(id: Long)     // для удаления
+
+}
